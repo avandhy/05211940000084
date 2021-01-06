1b3041cdf79fb4ff723f80e45e41f7c6adf0dd72
avandhy putra <70370100+avandhy@users.noreply.github.com>
2021-01-06 20:32:35 +0700

    perbaikan halaman read

--- resources/views/pegawai/read.blade.php
+++ resources/views/pegawai/read.blade.php
@@ -2,33 +2,48 @@
 
 @section('content')
 <button type="submit" class="btn btn-primary" style="margin: 2rem 0"><a class="link" href="/pegawai">Kembali</a></button>
-
-
 @foreach($pegawai as $p)
+<div style="font-size:1.25rem">
+    <h3>Data Pegawai</h3>
     <div hidden name="id">{{ $p->pegawai_id }}</div><br>
-    <div for="nama">Nama:{{ $p->pegawai_nama }}</div><br>
-    <div for="jabatan">Jabatan:{{ $p->pegawai_jabatan }}</div><br>
-    <div for="umur">Umur:{{ $p->pegawai_umur }}</div><br>
-    <div for="alamat">Alamat:{{ $p->pegawai_alamat }}</div><br>
+    <div class="row">
+        <div class="col-lg-4">Nama :</div>
+        <div class="col-lg-4">{{ $p->pegawai_nama }}</div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">Jabatan :</div>
+        <div class="col-lg-4">{{ $p->pegawai_jabatan }}</div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">Umur :</div>
+        <div class="col-lg-4">{{ $p->pegawai_umur }}</div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">Alamat :</div>
+        <div class="col-lg-4">{{ $p->pegawai_alamat }}</div>
+    </div>
+</div>
 @endforeach
 
-<h3>Daftar Tugas</h3>
-<table class="table table-hover" style="margin: 5rem 0">
-    <tr>
-        <th>Waktu</th>
-        <th>Tugas</th>
-        <th>Status</th>
-    </tr>
-    @foreach($tugas as $t)
-        @if($p->pegawai_id==$t->IDPegawai)
+<div style="margin: 5rem 0">
+    <h3>Daftar Tugas</h3>
+    <table class="table table-hover" style="margin: 2rem 0">
         <tr>
-            <td>{{ $t->	Tanggal }}</td>
-            <td>{{ $t->NamaTugas }}</td>
-            <td>{{ $t->	Status }}</td>
-         </tr>
-        @endif
-    @endforeach
-</table>
+            <th>Waktu</th>
+            <th>Tugas</th>
+            <th>Status</th>
+        </tr>
+        @foreach($tugas as $t)
+            @if($p->pegawai_id==$t->IDPegawai)
+            <tr>
+                <td>{{ $t->	Tanggal }}</td>
+                <td>{{ $t->NamaTugas }}</td>
+                <td>{{ $t->	Status }}</td>
+            </tr>
+            @endif
+        @endforeach
+    </table>
+</div>
 @endsection
 
 @section('style')
